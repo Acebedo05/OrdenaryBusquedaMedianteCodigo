dc0cf374cef39cd82786a5668d29bf3069ac1831
jaceara2006 <jaceara2006@g.educaand.es>
2023-12-19 13:49:24 +0100

    CompararArray y BinarySearch

--- src/main/java/daw/Ej1.java
+++ src/main/java/daw/Ej1.java
@@ -1,6 +1,7 @@
 package daw;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -61,7 +62,7 @@ public class Ej1 {
         }
 
         System.out.println("----------------");
-        
+
         // Genero un array int con valores aleatorios.
         int[] arrayAleatorio = new int[TAMANO];
         llenarArrayAleatorio(arrayAleatorio);
@@ -71,12 +72,11 @@ public class Ej1 {
 //        System.out.println("----------------");
 //        System.out.println("Array ordenado: " + Arrays.toString(arrayAleatorio) + ".");
 //        System.out.println("----------------");
-
         // Ordeno el array (Burbuja).
         int tpm;
         for (int i = 0; i < arrayAleatorio.length; i++) {
-            for (int j = i+1; j < arrayAleatorio.length; j++) {
-                if (arrayAleatorio[i] > arrayAleatorio[j]){
+            for (int j = i + 1; j < arrayAleatorio.length; j++) {
+                if (arrayAleatorio[i] > arrayAleatorio[j]) {
                     tpm = arrayAleatorio[i];
                     arrayAleatorio[i] = arrayAleatorio[j];
                     arrayAleatorio[j] = tpm;
@@ -84,7 +84,7 @@ public class Ej1 {
             }
         }
         System.out.println("----------------");
-        System.out.println("Array ordenador: " + Arrays.toString(arrayAleatorio));
+        System.out.println("Array ordenado: " + Arrays.toString(arrayAleatorio));
         System.out.println("----------------");
 
         // Preguntar al usuario.
@@ -95,11 +95,29 @@ public class Ej1 {
         int ubicacion = Arrays.binarySearch(arrayAleatorio, numeroBuscar);
 
         if (ubicacion < 0) {
-            System.out.println("El elemento no se encuentra en el array.");
+            System.out.println("El numero no se encuentra en el array.");
         } else {
-            System.out.println("El elemento se encuentra en la posición " + ubicacion + " del array.");
+            System.out.println("El numero se encuentra en la posición " + ubicacion + " del array.");
         }
 
+        System.out.println("----------------");
+        System.out.println("----------------");
+        boolean igualarrays = compararArray(miarray, miarraycopia);
+        if (igualarrays == true) {
+            System.out.println("Ambos arrays son iguales.");
+        } else {
+            System.out.println("Ambos arrays son diferentes.");
+        }
+        System.out.println("----------------");
+        System.out.println("----------------");
+
+        int resultado = binarySearch(arrayAleatorio);
+
+        if (resultado != -1) {
+            System.out.println("El numero se encuentra en la posición " + resultado + " del array.");
+        } else {
+            System.out.println("El numero no se encuentra en el array.");
+        }
     }
 
     public static void llenarArrayAleatorio(int[] array) {
@@ -110,4 +128,50 @@ public class Ej1 {
             array[i] = aleatorio;
         }
     }
+
+    public static boolean compararArray(double[] arrayUno, double[] arrayDos) {
+        boolean igual = false;
+
+        for (int i = 0; i < arrayUno.length; i++) {
+            if (arrayUno[i] == arrayDos[i]) {
+                igual = true;
+            } else {
+                igual = false;
+                break;
+            }
+        }
+
+        return igual;
+    }
+
+    public static int binarySearch(int[] array) {
+        // Inicializo las variables.
+        int mitad = 0;
+        int izquierda = 0;
+        int derecha = array.length - 1;
+        boolean numEncotrado = false;
+        int index = -1;
+
+        System.out.println("Array ordenado: " + Arrays.toString(array));
+        System.out.println("----------------");
+
+        // Preguntar al usuario.
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número a buscar: ");
+        int numeroBuscar = scanner.nextInt();
+
+        while ((izquierda <= derecha) && (!numEncotrado)) {
+            mitad = (izquierda + derecha) / 2;
+            if (array[mitad] == numeroBuscar) {
+                numEncotrado = true;
+                index = mitad;
+            } else if (array[mitad] > numeroBuscar) {
+                derecha = mitad - 1;
+            } else {
+                izquierda = mitad + 1;
+            }
+        }
+        return index;
+    }
+
 }
